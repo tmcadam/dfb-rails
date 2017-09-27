e4c1cebf1ca57dcc9809ee0f7a487b9088ae24a9
Tom McAdam <tmcadam@users.noreply.github.com>
2017-09-27 12:27:30 +0900

    Added an index page showing all biographies, will be used for search results later

--- app/controllers/biographies_controller.rb
+++ app/controllers/biographies_controller.rb
@@ -1,5 +1,11 @@
 class BiographiesController < ApplicationController
+
     def show
-      @biography = Biography.find_by(slug: params[:id]) || Biography.find(params[:id])
+        @biography = Biography.find_by(slug: params[:id]) || Biography.find(params[:id])
     end
+
+    def index
+        @biographies = Biography.all
+    end
+
 end
--- test/controllers/biographies_controller_test.rb
+++ test/controllers/biographies_controller_test.rb
@@ -10,6 +10,7 @@ class BiographiesControllerTest < ActionDispatch::IntegrationTest
     test "can show biography using slug" do
         get biography_path("slug_name1") #uses one of the fixtures
         assert_response :success
+
     end
 
     test "404 if biography id or slug not found" do
@@ -17,4 +18,17 @@ class BiographiesControllerTest < ActionDispatch::IntegrationTest
         assert_raises(ActiveRecord::RecordNotFound) {get biography_path("slug_typo")}
     end
 
+    test "can show biographies index" do
+        get biographies_path
+        assert_response :success
+    end
+
+    test "index route returns all biographies" do
+        get biographies_path
+        biographies = @controller.index
+        assert_equal(biographies.length, 2)
+        assert_select 'table#index' do
+            assert_select 'tr', 2
+        end
+    end
 end
