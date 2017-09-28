696d14bd2d29bb2c676c05900601790bf3ccc8d9
Tom McAdam <tmcadam@users.noreply.github.com>
2017-09-28 16:43:50 +0900

    Took set commands out of travis deploy script

--- travis-webfaction-deploy.sh
+++ travis-webfaction-deploy.sh
@@ -5,8 +5,8 @@ echo "Deploying to Webfaction"
 if [ ${1} = "staging" ];
 then
     echo "Deploying to staging server"
-    set DEPLOY_PATH=$DEPLOY_PATH_STAGING
-    set DEPLOY_BRANCH=$DEPLOY_BRANCH_STAGING
+    DEPLOY_PATH=$DEPLOY_PATH_STAGING
+    DEPLOY_BRANCH=$DEPLOY_BRANCH_STAGING
 else
     echo "Deploying to production server"
 fi
@@ -14,5 +14,3 @@ fi
 export SSHPASS=$DEPLOY_PASS
 sshpass -e ssh -o stricthostkeychecking=no ${DEPLOY_USER}@${DEPLOY_HOST} bash "${DEPLOY_HOME}/git_deploy.sh" "${DEPLOY_PATH}" ${DEPLOY_REPO} ${DEPLOY_BRANCH}
 sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST bash "${DEPLOY_PATH}/deploy.sh"
-
-echo "Webfaction deployment successful"
