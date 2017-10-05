3398e48aa783bda3fe42b73f96a9f4cc8fc91942
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-05 22:28:13 +0900

    Changed text field to body in statc_content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005111907) do
+ActiveRecord::Schema.define(version: 20171005114651) do
 
   create_table "biographies", force: :cascade do |t|
     t.string "title"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20171005111907) do
 
   create_table "static_contents", force: :cascade do |t|
     t.string "title"
-    t.text "text"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
