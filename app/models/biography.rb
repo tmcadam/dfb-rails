a2fb68c0d08d52ae0354f55722521de5d567aea5
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-08 10:30:49 +0900

    Added simple images to body content dynamically

--- app/models/biography.rb
+++ app/models/biography.rb
@@ -1,4 +1,25 @@
 class Biography < ApplicationRecord
+    has_many :images
     validates :slug, presence: true, uniqueness: true
     default_scope { order(title: :asc) }
+
+    def body_with_images
+        paras = Nokogiri::HTML.parse(self.body).css('p')
+        output = paras.slice(0,1).map {|x| x.to_html}.compact.join
+        current_para = 1
+        position = "right"
+        self.images.each do |image|
+            if position == "right"
+                pos_class = "biography-img-right"
+                position = "left"
+            else
+                pos_class = "biography-img-left"
+                position = "right"
+            end
+            output += ActionController::Base.helpers.image_tag image.image.url(:medium), class: pos_class
+            output += paras.slice(current_para,2).map {|x| x.to_html}.compact.join
+            current_para += 2
+        end
+        output += paras.slice(current_para, paras.length).map {|x| x.to_html}.compact.join
+    end
 end
