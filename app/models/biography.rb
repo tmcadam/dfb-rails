3e22d255e7e99ffa991ced29bdad5ced6ca66d1d
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-08 19:56:45 +0900

    Another attempt at a method to insert images to body

--- app/models/biography.rb
+++ app/models/biography.rb
@@ -4,22 +4,45 @@ class Biography < ApplicationRecord
     default_scope { order(title: :asc) }
 
     def body_with_images
-        paras = Nokogiri::HTML.parse(self.body).css('p')
-        output = paras.slice(0,1).map {|x| x.to_html}.compact.join
-        current_para = 1
-        position = "right"
-        self.images.each do |image|
-            if position == "right"
-                pos_class = "biography-img-right"
-                position = "left"
-            else
-                pos_class = "biography-img-left"
-                position = "right"
+        output = ""
+        images = generate_image_tags
+        p_counter = 0
+        get_body_elements.each do |element, index|
+            output += element.to_html
+            if element.text.length > 200
+                p_counter += 1
+                output += insert_image(p_counter, images)
             end
-            output += ActionController::Base.helpers.image_tag image.image.url(:medium), class: pos_class
-            output += paras.slice(current_para,2).map {|x| x.to_html}.compact.join
-            current_para += 2
         end
-        output += paras.slice(current_para, paras.length).map {|x| x.to_html}.compact.join
+        output
     end
+
+private
+
+    def insert_image(p, images)
+        images.each do |img|
+            if img["after_para"] == p
+                return img["tag"]
+            end
+        end
+        return String.new
+    end
+
+    def get_body_elements
+        Nokogiri::HTML::fragment(self.body).children
+    end
+
+    def generate_image_tags
+        tags = []
+        after_para = 1
+        self.images.each do |image|
+            pos_class = ApplicationController.helpers.cycle("biography-img-right", "biography-img-left", name: "pos_class")
+            tag = ApplicationController.helpers.image_tag image.image.url(:medium), class: pos_class
+            tags.push({"tag"=>tag, "after_para"=>after_para})
+            after_para += 2
+        end
+        ApplicationController.helpers.reset_cycle("pos_class")
+        tags
+    end
+
 end
