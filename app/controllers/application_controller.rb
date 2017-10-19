a2a78da5fa21025f7af67957375581aafc17203c
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-19 10:02:33 +0900

    Changed how editting is blocked in prod and staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+
+    protected
+        def restrict_to_development
+            head(:bad_request) unless Rails.env.development? or Rails.env.test?
+        end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  if Rails.env.development? or Rails.env.test?
-      resources :images
-      resources :biographies
-  else
-      resources :biographies, only: [:show]
-  end
+  resources :images
+  resources :biographies
   resources :static_content, only: [:show], path: "/", param: :slug
   root :to => redirect('/home')
 end
