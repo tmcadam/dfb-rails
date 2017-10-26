0cb720807ccb315e72efbfc4a497c43033f3de8d
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-26 12:07:51 +0900

    Ran authors migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018110324) do
+ActiveRecord::Schema.define(version: 20171026030412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.text "biography"
+    t.text "contributions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "biographies", force: :cascade do |t|
     t.string "title"
