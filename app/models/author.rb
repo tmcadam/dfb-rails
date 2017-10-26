900b124aabde6053d3188f6913a9534ac7a44e42
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-26 12:52:19 +0900

    Added some authors models validations and tests

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :contributions, presence: true
 end
--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -1,9 +1,34 @@
 require "test_helper"
 
-describe Author do
-  let(:author) { Author.new }
+class AuthorTest < ActiveSupport::TestCase
+    setup do
+        DatabaseCleaner.clean
+        @a1 = Author.new(name:"Author1", biography:"Biography of author", contributions:"Authors contributions")
+        @a2 = Author.new(name:"Author2", biography:"Biography of author", contributions:"Authors contributions")
+    end
 
-  it "must be valid" do
-    value(author).must_be :valid?
-  end
+    test "valid author is valid with all attributes present and correct" do
+        assert @a1.valid?
+    end
+
+    test "can save valid author to db" do
+        assert @a1.save
+    end
+
+    test "author without name is invalid" do
+        @a1.name = nil
+        assert_not @a1.valid?
+    end
+
+    test "author without contributions is invalid" do
+        @a1.contributions = nil
+        assert_not @a1.valid?
+    end
+
+    test "author with duplicatename is invalid" do
+        @a1.save
+        @a2.name = "Author1"
+        assert_not @a2.valid?
+    end
+
 end
