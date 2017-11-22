f4905071b3632e9db6ec5c1769fd49349d1f8070
Tom McAdam <tmcadam@users.noreply.github.com>
2017-11-22 11:04:56 +0900

    Added a 'new' form for static content

--- test/controllers/static_content_controller_test.rb
+++ test/controllers/static_content_controller_test.rb
@@ -82,15 +82,68 @@ class StaticContentControllerTest < ActionDispatch::IntegrationTest
         assert_redirected_to new_user_session_path
     end
 
-    test "renders 'Edit' button if logged in" do
+    test "renders 'Edit','Delete' buttons and 'New' menu if logged in" do
         sign_in @u1
         get static_content_path(@home)
         assert_select "a.btn[href=?]", edit_static_content_path(@home), {:text => "Edit"}
+        assert_select "a.btn[href=?]", static_content_path(@home), {:text => "Delete", :method => :DELETE}
+        assert_select "a[href=?]", new_static_content_path
     end
 
     test "doesn't render edit button if not logged in" do
         get static_content_path(@home)
         assert_select "a.btn", false, {:text => "Edit"}
+        assert_select "a.btn", false, {:text => "Delete"}
+        assert_select "a[href=?]", new_static_content_path, false
+    end
+
+    test "can render a create new static_content form, if logged in" do
+        sign_in @u1
+        get new_static_content_path
+        assert_response :success
+        assert_select "input#static_content_title"
+        assert_select "input#static_content_slug"
+        assert_select "textarea#static_content_body"
+        assert_select "input#static_content_title[value]", false
+        assert_select "input#static_content_slug[value]", false
+        assert_select "textarea#static_content_body[value]", false
+    end
+
+    test "can not render new static content form if not logged in" do
+        get new_static_content_path
+        assert_redirected_to new_user_session_path
+    end
+
+    test "can create new static page if logged in" do
+        sign_in @u1
+        assert_difference('StaticContent.count') do
+            post static_content_index_path(static_content:{title:"New page", slug:"new_page", body:"A great page"})
+        end
+        assert_redirected_to static_content_path("new_page")
+    end
+
+    test "can not create new static content if not logged in" do
+        assert_no_difference('StaticContent.count') do
+            post static_content_index_path(static_content:{title:"New page", slug:"new_page", body:"A great page"})
+        end
+        assert_redirected_to new_user_session_path
+    end
+
+    test "can destroy static_content if logged in" do
+        sign_in @u1
+        @sc = StaticContent.create(title:"New page", slug:"new_page", body:"A great page")
+        assert_difference('StaticContent.count', -1) do
+            delete static_content_path(@sc)
+        end
+        assert_redirected_to static_content_path("home")
+    end
+
+    test "can not delete if not logged in" do
+        @sc = StaticContent.create(title:"New page", slug:"new_page", body:"A great page")
+        assert_no_difference('StaticContent.count') do
+            delete static_content_path(@sc)
+        end
+        assert_redirected_to new_user_session_path
     end
 
     teardown do
