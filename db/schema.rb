5434220488c31539b002a0e88fd2c7282f7041bb
Tom McAdam <mcadam1980@gmail.com>
2018-01-29 12:43:25 +0900

    Added external_links field to biographies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117082051) do
+ActiveRecord::Schema.define(version: 20180129033732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20180117082051) do
     t.integer "secondary_country_id"
     t.boolean "south_georgia"
     t.boolean "featured"
+    t.text "external_links"
     t.index ["slug"], name: "index_biographies_on_slug"
   end
 
