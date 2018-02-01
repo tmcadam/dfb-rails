7c753ea243e08f95542d2a4c07076f8b2fccfb96
Tom McAdam <mcadam1980@gmail.com>
2018-02-01 11:18:15 +0900

    All the things for admin to view and approve comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,30 @@
 class CommentsController < ApplicationController
 
+    before_action :authenticate_user!, :only => [:index, :edit, :update, :destroy]
+
+    def index
+        @comments = Comment.all.order(:created_at).reverse_order.page params[:page]
+    end
+
+    def edit
+        @comment = Comment.find( params[:id] )
+    end
+
+    def destroy
+        @comment = Comment.find( params[:id] )
+        @comment.destroy
+        redirect_to comments_path, :flash => { :notice => "Comment successfully deleted." }
+    end
+
+    def update
+        @comment = Comment.find( params[:id] )
+        if @comment.update( comment_params )
+            redirect_to comments_path, :flash => { :notice => "Comment successfully updated." }
+        else
+            render 'edit'
+        end
+    end
+
     def create
         @comment = Comment.new( comment_params_public )
         @comment.approved = false
@@ -16,6 +41,10 @@ private
 
     def comment_params_public
         params.require(:comment).permit(:biography_id, :name, :email, :comment)
+    end
+
+    def comment_params
+        params.require(:comment).permit(:biography_id, :name, :email, :comment, :approved)
     end
 
 end
