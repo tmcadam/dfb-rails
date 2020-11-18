6035b8cad7197e2b98ec9fc4ceda25eb31f1c460
Tom McAdam <mcadam1980@mailcom>
2020-11-18 10:59:59 +1000

    Deploy updates

--- travis-swb-deploy.sh
+++ travis-swb-deploy.sh
@@ -5,5 +5,5 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export SSHPASS=$DEPLOY_PASS
 sshpass -e scp -o stricthostkeychecking=no -P ${DEPLOY_PORT} "${PROJECT_DIR}/git-deploy.sh" ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_HOME}
-sshpass -e ssh -o stricthostkeychecking=no -p $DEPLOY_PORT $DEPLOY_USER@$DEPLOY_HOST sudo bash ${DEPLOY_HOME}/git-deploy.sh "${DEPLOY_PATH}" ${DEPLOY_REPO} ${TRAVIS_COMMIT}
-sshpass -e ssh -tt -o stricthostkeychecking=no -p $DEPLOY_PORT $DEPLOY_USER@$DEPLOY_HOST sudo docker container exec -it ${DEPLOY_CONT} bash -l "deploy.sh staging"
+sshpass -e ssh -o stricthostkeychecking=no -p $DEPLOY_PORT $DEPLOY_USER@$DEPLOY_HOST sudo bash ${DEPLOY_HOME}/git-deploy.sh "${DEPLOY_PATH}" ${TRAVIS_REPO_SLUG} ${TRAVIS_COMMIT}
+sshpass -e ssh -tt -o stricthostkeychecking=no -p $DEPLOY_PORT $DEPLOY_USER@$DEPLOY_HOST sudo docker container exec -it ${DEPLOY_CONT} bash -l "deploy.sh"
