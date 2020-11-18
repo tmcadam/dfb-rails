46d4475b5e01eb177d0c1562c9ff95863028f14b
Tom McAdam <mcadam1980@mailcom>
2020-11-18 10:46:41 +1000

    Deploy updates

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "Deploy: Changing file permissions"
 cd $APP_DIR
 chmod +x -R $APP_DIR
 chown -R app:app $APP_DIR
-#chmod -R 0666 $APP_DIR/log
 
+echo "Deploy: Installing dependencies"
 bundle install
+echo "Deploy: Applying migrations"
 rake db:migrate
+echo "Deploy: Compiling assets"
 bundle exec rake assets:precompile
--- travis-swb-deploy.sh
+++ travis-swb-deploy.sh
@@ -6,4 +6,4 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export SSHPASS=$DEPLOY_PASS
 sshpass -e scp -o stricthostkeychecking=no -P ${DEPLOY_PORT} "${PROJECT_DIR}/git-deploy.sh" ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_HOME}
 sshpass -e ssh -o stricthostkeychecking=no -p $DEPLOY_PORT $DEPLOY_USER@$DEPLOY_HOST sudo bash ${DEPLOY_HOME}/git-deploy.sh "${DEPLOY_PATH}" ${DEPLOY_REPO} ${TRAVIS_COMMIT}
-sshpass -e ssh -o stricthostkeychecking=no -p $DEPLOY_PORT $DEPLOY_USER@$DEPLOY_HOST sudo bash ${DEPLOY_PATH}/deploy.sh
+sshpass -e ssh -tt -o stricthostkeychecking=no -p $DEPLOY_PORT $DEPLOY_USER@$DEPLOY_HOST sudo docker container exec -it ${DEPLOY_CONT} bash -l "deploy.sh staging"
