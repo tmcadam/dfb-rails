db209fc4c0a0ac9576c909909bff2087f9b7d31e
Tom McAdam <mcadam1980@gmail.com>
2021-04-29 22:32:37 +1000

    Added environment variables to test emails in dev environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'dfb-staging.ukfit.webfactional.com' }
+  config.action_mailer.default_url_options = { :host => 'staging.falklandsbiographies.org' }
   config.admin_comment_email = ENV["DFB_COMMENTS_EMAIL_STAGING"]
 end
