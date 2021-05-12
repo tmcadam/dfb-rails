f827182c624e94096f177f16195ed411ae674255
Tom McAdam <mcadam1980@gmail.com>
2021-05-13 08:58:23 +1000

    Schema change generated by rails

--- db/schema.rb
+++ db/schema.rb
@@ -2,11 +2,11 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 2019_08_29_103139) do
     t.string "title"
     t.text "caption"
     t.string "attribution"
-    t.integer "biography_id"
+    t.bigint "biography_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_file_name"
@@ -116,4 +116,5 @@ ActiveRecord::Schema.define(version: 2019_08_29_103139) do
   add_foreign_key "biographies", "countries", column: "primary_country_id"
   add_foreign_key "biographies", "countries", column: "secondary_country_id"
   add_foreign_key "comments", "biographies"
+  add_foreign_key "images", "biographies"
 end
