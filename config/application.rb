47d8c75da234b052ddb22c69a5a1cbb13b1e17f7
Tom McAdam <mcadam1980@gmail.com>
2026-02-06 04:06:43 +0000

    Make all mailer settings driven from env vars

--- config/application.rb
+++ config/application.rb
@@ -18,12 +18,12 @@ module DfbRails
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_caching = false
     config.action_mailer.smtp_settings = {
-        :address              => "smtp.gmail.com",
-        :domain               => "gmail.com",
-        :port                 => 587,
+        :address              => ENV["SMTP_HOST"],
+        :domain               => ENV["SMTP_DOMAIN"],
+        :port                 => ENV["SMTP_PORT"].to_i,
         :user_name            => ENV["SMTP_USER"],
         :password             => ENV["SMTP_PASS"],
-        :authentication       => "login",
+        :authentication       => :login,
         :enable_starttls_auto => true
     }
     # Settings in config/environments/* take precedence over those specified here.
